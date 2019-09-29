0ecdbe908fe57d32595a81fa232686b79d72d2d7
Cristobal Eyzaguirre <ceyzaguirre4@uc.cl>
2019-09-29 20:53:02 -0300

    wip, many fixes using original (tensorflow) implementation

--- model.py
+++ model.py
@@ -18,9 +18,10 @@ class ControlUnit(nn.Module):
         super().__init__()
         self.cfg = cfg
 
+        self.shared_control_proj = linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
         self.position_aware = nn.ModuleList()
         for i in range(cfg.MAC.MAX_ITER):
-            self.position_aware.append(linear(cfg.MAC.DIM * 2, cfg.MAC.DIM))
+            self.position_aware.append(linear(cfg.MAC.DIM, cfg.MAC.DIM))    # if controlInputUnshared
 
         self.control_question = linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
         self.attn = linear(cfg.MAC.DIM, 1)
@@ -30,6 +31,8 @@ class ControlUnit(nn.Module):
     def forward(self, context, question, controls):
         cur_step = len(controls) - 1
         control = controls[-1]
+
+        question = F.tanh(self.shared_control_proj(question))
         position_aware = self.position_aware[cur_step](question)
 
         control_question = torch.cat([control, position_aware], 1)
@@ -37,10 +40,16 @@ class ControlUnit(nn.Module):
         control_question = control_question.unsqueeze(1)
 
         context_prod = control_question * context
+
+        # ++ optionally concatenate words (= context)
+
+        # optional projection (if config.controlProj) --> stacks another linear after activation
+
         attn_weight = self.attn(context_prod)
 
         attn = F.softmax(attn_weight, 1)
 
+        # only valid if self.inwords == self.outwords
         next_control = (attn * context).sum(1)
 
         return next_control
@@ -53,15 +62,35 @@ class ReadUnit(nn.Module):
 
         self.mem_proj = linear(cfg.MAC.DIM, cfg.MAC.DIM)
         self.kb_proj = linear(cfg.MAC.DIM, cfg.MAC.DIM)
-        self.concat = linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
+        self.concat = linear(cfg.MAC.DIM * 3, cfg.MAC.DIM)
+        self.concat2 = linear(cfg.MAC.DIM, cfg.MAC.DIM)
         self.attn = linear(cfg.MAC.DIM, 1)
 
     def forward(self, memory, know, control):
+        # TODO: dropout(memory, self.dropouts["memory"])
+
+        ## Step 1: knowledge base / memory interactions 
+        # TODO: dropout memory and know (again?)
         proj_mem = self.mem_proj(memory[-1]).unsqueeze(2)
         proj_know = self.kb_proj(know.permute(0,2,1)).permute(0,2,1)
-        concat = self.concat(torch.cat([proj_mem * proj_know, know], 1) \
-                                .permute(0, 2, 1))
+        concat = torch.cat([
+                proj_mem * proj_know, 
+                proj_know,      # readMemConcatKB
+                proj_mem        # readMemConcatProj (this also makes the know above be the projection)
+                ], 1)
+        # project memory interactions back to hidden dimension
+        concat = self.concat2(F.relu(self.concat(concat).permute(0, 2, 1)))       # if readMemProj ++ second projection and nonlinearity if readMemAct 
+
+        ## Step 2: compute interactions with control (if config.readCtrl)
         attn = concat * control[-1].unsqueeze(1)
+
+        # if readCtrlConcatInter torch.cat([interactions, concat])
+
+        # optionally concatenate knowledge base elements
+
+        # optional nonlinearity 
+
+        # TODO: dropout attn before here
         attn = self.attn(attn).squeeze(2)
         attn = F.softmax(attn, 1).unsqueeze(1)
 
@@ -74,29 +103,51 @@ class WriteUnit(nn.Module):
     def __init__(self, cfg):
         super().__init__()
 
-        self.concat = linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
-
         if cfg.MAC.SELF_ATT:
+            self.control = linear(cfg.MAC.DIM, cfg.MAC.DIM)
             self.attn = linear(cfg.MAC.DIM, 1)
-            self.W_s = linear(cfg.MAC.DIM, cfg.MAC.DIM)
-            self.W_p = linear(cfg.MAC.DIM, cfg.MAC.DIM)
+            self.concat = linear(cfg.MAC.DIM * 3, cfg.MAC.DIM)
+        else:
+            self.concat = linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
 
         self.self_attention = cfg.MAC.SELF_ATT
 
     def forward(self, memories, retrieved, controls):
-        prev_mem = memories[-1]
-        concat = self.concat(torch.cat([retrieved, prev_mem], 1))
-        next_mem = concat
+        # optionally project info
 
+        # optional info nonlinearity
+
+        # compute self-attention vector based on previous controls and memories
         if self.self_attention:
+            selfControl = controls[-1]
+            selfControl = self.control(selfControl)
             controls_cat = torch.stack(controls[:-1], 2)
-            attn = controls[-1].unsqueeze(2) * controls_cat
+            attn = selfControl.unsqueeze(2) * controls_cat
             attn = self.attn(attn.permute(0, 2, 1))
             attn = F.softmax(attn, 1).permute(0, 2, 1)
 
             memories_cat = torch.stack(memories, 2)
             attn_mem = (attn * memories_cat).sum(2)
-            next_mem = self.W_s(attn_mem) + self.W_p(concat)
+            # next_mem = self.W_s(attn_mem) + self.W_p(concat)
+
+
+        prev_mem = memories[-1]
+        # get write unit inputs: previous memory, the new info, optionally self-attention / control
+        concat = torch.cat([retrieved, prev_mem], 1)
+
+        if self.self_attention:
+            concat = torch.cat([concat, attn_mem], 1)
+
+        # project memory back to memory dimension
+        concat = self.concat(concat)
+
+        # optional memory nonlinearity
+
+        # write unit gate moved to RNNWrapper
+
+        # optional batch normalization
+
+        next_mem = concat
 
         return next_mem
 
@@ -110,6 +161,7 @@ class MACCell(nn.Module):
         self.write = WriteUnit(cfg)
 
         self.mem_0 = nn.Parameter(torch.zeros(1, cfg.MAC.DIM))
+        # TODO: control0 is most often question, other times (eg. args2.txt) its a learned parameter initialized as random normal
         self.control_0 = nn.Parameter(torch.zeros(1, cfg.MAC.DIM))
 
         self.dim = cfg.MAC.DIM
@@ -157,16 +209,31 @@ class MACCell(nn.Module):
 
         return controls, memories
 
+class OutputUnit(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.question_proj = nn.Linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
+        self.classifier = nn.Sequential(nn.Dropout(0.15),       # output dropout
+                                        nn.Linear(cfg.MAC.DIM * 2, cfg.MAC.DIM),
+                                        nn.ReLU(),
+                                        nn.Dropout(0.15),       # output dropout
+                                        nn.Linear(cfg.MAC.DIM, cfg.OUTPUT.DIM))
+        xavier_uniform_(self.classifier_out[1].weight)
+        xavier_uniform_(self.classifier_out[4].weight)
+    
+    def forward(self, last_mem, question):
+        question = self.question_proj(question)
+        cat = torch.cat([last_mem, question], 1)
+        out = self.classifier_out(cat)
+        return out
+
 
 class RecurrentWrapper(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.controller = MACCell(cfg)
-        self.classifier = nn.Sequential(linear(cfg.MAC.DIM * 3, cfg.MAC.DIM),
-                                        nn.ELU(),
-                                        linear(cfg.MAC.DIM, cfg.OUTPUT.DIM))
-        kaiming_uniform_(self.classifier[0].weight)
-        kaiming_uniform_(self.classifier[2].weight)
+        self.classifier_out = OutputUnit(cfg)
 
         self.cfg = cfg
         # gate with xavier uniform
@@ -186,8 +253,7 @@ class RecurrentWrapper(nn.Module):
         
         _, memories = state
 
-        out = torch.cat([memories[-1], inputs[1]], 1)
-        out = self.classifier(out)
+        out = self.classifier_out(memories[-1], inputs[1])
 
         return out
 
@@ -195,15 +261,17 @@ class RecurrentWrapper(nn.Module):
 class MACNetwork(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.conv = nn.Sequential(nn.Conv2d(1024, cfg.MAC.DIM, 3, padding=1),
-                                nn.ELU(),
-                                nn.Conv2d(cfg.MAC.DIM, cfg.MAC.DIM, 3, padding=1),
-                                nn.ELU())
+        self.conv = nn.Sequential(
+            nn.Dropout(p=0.18),     # stem dropout
+            nn.Conv2d(1024, cfg.MAC.DIM, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout(p=0.18),     # stem dropout
+            nn.Conv2d(cfg.MAC.DIM, cfg.MAC.DIM, 3, padding=1),
+            nn.ELU())
 
         self.embed = nn.Embedding(cfg.INPUT.N_VOCAB, cfg.MAC.EMBD_DIM)
         self.lstm = nn.LSTM(cfg.MAC.EMBD_DIM, cfg.MAC.DIM,
                         batch_first=True, bidirectional=True)
-        self.lstm_proj = nn.Linear(cfg.MAC.DIM * 2, cfg.MAC.DIM)
 
         # choose different wrappers for no-act/actSmooth/actBaseline
         self.actmac = RecurrentWrapper(cfg)
@@ -212,11 +280,12 @@ class MACNetwork(nn.Module):
         self.reset()
 
     def reset(self):
-        self.embed.weight.data.uniform_(0, 1)
+        # from original implementation
+        self.embed.weight.data.xavier_uniform_()
 
-        kaiming_uniform_(self.conv[0].weight)
+        xavier_uniform_(self.conv[0].weight)
         self.conv[0].bias.data.zero_()
-        kaiming_uniform_(self.conv[2].weight)
+        xavier_uniform_(self.conv[2].weight)
         self.conv[2].bias.data.zero_()
 
     def forward(self, image, question, question_len, dropout=0.15):
@@ -226,14 +295,16 @@ class MACNetwork(nn.Module):
         img = img.view(b_size, self.dim, -1)
 
         embed = self.embed(question)
+        # embed = self.embedding_dropout(embed)
         embed = nn.utils.rnn.pack_padded_sequence(embed, question_len,
                                                 batch_first=True)
         lstm_out, (h, _) = self.lstm(embed)
+        question = torch.cat([h[0], h[1]], -1)
+        # h = self.question_dropout(h)      # TODO
         lstm_out, _ = nn.utils.rnn.pad_packed_sequence(lstm_out,
                                                     batch_first=True)
-        lstm_out = self.lstm_proj(lstm_out)
         h = h.permute(1, 0, 2).contiguous().view(b_size, -1)
 
-        out = self.actmac(lstm_out, h, img)
+        out = self.actmac(lstm_out, question, img)
 
         return out
